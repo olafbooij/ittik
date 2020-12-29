37d2a8c8e487f9671228e82472462b93fc82e300
O B thuis <olaf.booij@xs4all.nl>
2020-12-29 22:27:11 +0100

    rename. started functionality to actually uncalib a sweep

---
+++ undistored_sweep.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+#include<calibration_kitti.hpp>
+#include<unapply_calibration_sweep.hpp>
+
+int main(/*int argc, char* argv[]*/)
+{
+  using namespace std;
+
+  return 0;
+}
+
