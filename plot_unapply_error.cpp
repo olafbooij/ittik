f1fc07dc446d926879514b0291786ff663733dd5
O B thuis <olaf.booij@xs4all.nl>
2021-01-26 23:19:57 +0100

    computed mean errors

--- plot_unapply_error.cpp
+++ plot_unapply_error.cpp
@@ -9,10 +9,14 @@ int main(int argc, char* argv[])
   SweepUncalibrator sweepUncalibrator;
 
   std::ifstream sweepFile(argv[1]);
-  std::ofstream outFile(argv[2]);
+  //std::ofstream outFile(argv[2]);
   Eigen::Vector3d point;
   double refl;
 
+  double distance_error = 0;
+  double angle_error = 0;
+  double norm = 0;
+  std::size_t n = 0;
   while(sweepFile >> point(0) >> point(1) >> point(2) >> refl)
   {
     auto [probeId, position, distanceUncor, vertId_] = sweepUncalibrator(point);
@@ -20,13 +24,20 @@ int main(int argc, char* argv[])
     auto pointAgain = apply_calibration(position, distanceUncor, kitti_probe_calibration().at(probeId));
     std::cout.precision(7);
     auto normrad = [](auto v){return v<M_PI?v:2*M_PI-v;};
-    outFile << probeId << " " << point.transpose() << " " << position << " " << distanceUncor << " " << (point - pointAgain).norm()
-            << " " << fabs(point.norm() - pointAgain.norm())
-            << " " << normrad(fabs(atan2(point(0), point(1)) - atan2(pointAgain(0), pointAgain(1))))
-            << std::endl;
-
+    //outFile << probeId << " " << point.transpose() << " " << position << " " << distanceUncor << " " << (point - pointAgain).norm()
+    //        << " " << fabs(point.norm() - pointAgain.norm())
+    //        << " " << normrad(fabs(atan2(point(0), point(1)) - atan2(pointAgain(0), pointAgain(1))))
+    //        << std::endl;
+    ++n;
+    auto runmean = [&n](auto& m, auto v){m = (1. - 1./n) * m + (1./n) * v;};
+    runmean(distance_error, fabs(point.norm() - pointAgain.norm()));
+    runmean(norm, (point - pointAgain).norm());
+    runmean(angle_error, normrad(fabs(atan2(point(0), point(1)) - atan2(pointAgain(0), pointAgain(1)))));
     // plot "unapply_error" using 
   }
+  std::cout << norm << std::endl;
+  std::cout << distance_error << std::endl;
+  std::cout << angle_error << std::endl;
 
   return 0;
 }
