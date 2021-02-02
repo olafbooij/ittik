895216fd44bff9808a63463f04d2c9adf7ded8c1
O B thuis <olaf.booij@xs4all.nl>
2021-02-02 23:50:10 +0100

    simplified the calibration functions

--- apply_calibration.hpp
+++ apply_calibration.hpp
@@ -3,9 +3,10 @@
 #include<Eigen/Core>
 #include<calibration_common.hpp>
 
-// Algorithm taken from the Coordinate Calculation Algorithm Sample Code from
-// Appendix F of the Velodyne HDL-64E S2 and S2.1 User's manual and programming
-// guide, revision 63HDL64E S2 Rev F DEC11
+// Part of the algorithm taken from the Coordinate Calculation Algorithm Sample
+// Code from Appendix F of the Velodyne HDL-64E S2 and S2.1 User's manual and
+// programming guide, revision 63HDL64E S2 Rev F DEC11
+// But without the x and y dependent distance correction.
 
 Eigen::Vector3d apply_calibration(const double position, const double distanceUncor, const ProbeCalibration& cal)
 {
@@ -19,24 +20,11 @@ Eigen::Vector3d apply_calibration(const double position, const double distanceUn
   auto cosVertAngle = cos(cal.vertCorrection);
   auto sinVertAngle = sin(cal.vertCorrection);
   double xyDistance = distance * cosVertAngle;
-  // Asuming calibration target was relative to lidar.
-  double xx = fabs(xyDistance * sinRotAngle - cal.horizOffsetCorrection * cosRotAngle);
-  double yy = fabs(xyDistance * cosRotAngle + cal.horizOffsetCorrection * sinRotAngle);
-
-  // These exact values are used to calibrate all units it seems.
-  double distanceCorrX = (cal.distCorrection - cal.distCorrectionX) * (xx - 2.40) / (25.04 - 2.40) + cal.distCorrectionX;
-  double distanceCorrY = (cal.distCorrection - cal.distCorrectionY) * (yy - 1.93) / (25.04 - 1.93) + cal.distCorrectionY;
 
   Eigen::Vector3d point;
-  auto distance_x_corrected = distanceUncor + distanceCorrX;
-  auto xyDistance_x_corrected = distance_x_corrected * cosVertAngle;
-  point(0) = xyDistance_x_corrected * sinRotAngle - cal.horizOffsetCorrection * cosRotAngle;
-
-  auto distance_y_corrected = distanceUncor + distanceCorrY;
-  auto xyDistance_y_corrected = distance_y_corrected * cosVertAngle;
-  point(1) = xyDistance_y_corrected * cosRotAngle + cal.horizOffsetCorrection * sinRotAngle;
-
-  point(2) = distance_y_corrected * sinVertAngle + cal.vertOffsetCorrection; // yes distance_y_corrected is used
+  point(0) = xyDistance * sinRotAngle - cal.horizOffsetCorrection * cosRotAngle;
+  point(1) = xyDistance * cosRotAngle + cal.horizOffsetCorrection * sinRotAngle;
+  point(2) = distance * sinVertAngle + cal.vertOffsetCorrection;
 
   return point;
 }
--- unapply_calibration_iter.hpp
+++ unapply_calibration_iter.hpp
@@ -11,45 +11,22 @@ auto unapply_calibration_iter(const Eigen::Vector3d point, const ProbeCalibratio
 {
   // initial estimate ignores most of the calibration
   auto rotCorrected = atan2(point(0), point(1));
-  auto position = rotCorrected + cal.rotCorrection;
-  auto distance = point.norm();
-  auto distanceUncor = distance - cal.distCorrection;
-  // iteratively update the position and distanceUncor estimate
+  // iteratively update the position estimate
   for(int i = 10; i--;) // ten fingers...
   {
-    // mostly copy-paste from forward apply_calibration
-    auto distance = distanceUncor + cal.distCorrection;
-    auto rotCorrected = position - cal.rotCorrection;
-    double cosRotAngle = cos(rotCorrected);
-    double sinRotAngle = sin(rotCorrected);
-
-    auto cosVertAngle = cos(cal.vertCorrection);
-    auto sinVertAngle = sin(cal.vertCorrection);
-    double xyDistance = distance * cosVertAngle;
-    double xx = fabs(xyDistance * sinRotAngle - cal.horizOffsetCorrection * cosRotAngle);
-    double yy = fabs(xyDistance * cosRotAngle + cal.horizOffsetCorrection * sinRotAngle);
-    double distanceCorrX = (cal.distCorrection - cal.distCorrectionX) * (xx - 2.40) / (25.04 - 2.40) + cal.distCorrectionX;
-    double distanceCorrY = (cal.distCorrection - cal.distCorrectionY) * (yy - 1.93) / (25.04 - 1.93) + cal.distCorrectionY;
-
-    auto distanceXCorrected = distanceUncor + distanceCorrX;
-    auto xyDistanceXCorrected = distanceXCorrected * cosVertAngle;
-    auto distanceYCorrected = distanceUncor + distanceCorrY;
-    auto xyDistanceYCorrected = distanceYCorrected * cosVertAngle;
-
-    // use the point to compute a more accurate position and distance
-    auto sinNewRotCorrected = (point(0) + cal.horizOffsetCorrection * cosRotAngle) / xyDistanceXCorrected;
-    auto cosNewRotCorrected = (point(1) - cal.horizOffsetCorrection * sinRotAngle) / xyDistanceYCorrected;
+    auto sinNewRotCorrected = point(0) + cal.horizOffsetCorrection * cos(rotCorrected);
+    auto cosNewRotCorrected = point(1) - cal.horizOffsetCorrection * sin(rotCorrected);
     rotCorrected = atan2(sinNewRotCorrected, cosNewRotCorrected);
-
-    if(fabs(point(0)) > fabs(point(1))) // picking the dimension with the largest value for accuracy
-      distanceUncor = (point(0) + cal.horizOffsetCorrection * cosRotAngle) / (cosVertAngle * sinRotAngle) - distanceCorrX;
-    else
-      distanceUncor = (point(1) - cal.horizOffsetCorrection * sinRotAngle) / (cosVertAngle * cosRotAngle) - distanceCorrY;
-
-    //distanceUncor = (point(2) - cal.vertOffsetCorrection) / sinVertAngle - distanceCorrY;
-
-    position = rotCorrected + cal.rotCorrection;
   }
+
+  auto cosVertAngle = cos(cal.vertCorrection);
+  auto distanceUncor = 0.;
+  if(fabs(point(0)) > fabs(point(1))) // picking the dimension with the largest value for accuracy
+    distanceUncor = (point(0) + cal.horizOffsetCorrection * cos(rotCorrected)) / (cosVertAngle * sin(rotCorrected)) - cal.distCorrection;
+  else
+    distanceUncor = (point(1) - cal.horizOffsetCorrection * sin(rotCorrected)) / (cosVertAngle * cos(rotCorrected)) - cal.distCorrection;
+
+  auto position = rotCorrected + cal.rotCorrection;
 
   return std::pair{position, distanceUncor};
 }
