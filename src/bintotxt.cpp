5d4f90b605943dd0eb5e1da12804c3579a406835
O B thuis <olaf.booij@xs4all.nl>
2021-02-13 17:24:18 +0100

    plotting semantics using the semantic kitti color scheme

--- src/bintotxt.cpp
+++ src/bintotxt.cpp
@@ -7,12 +7,12 @@ int main(int argc, char* argv[])
   std::ifstream binFile(argv[1], std::ios::in | std::ios::binary);
   std::ofstream txtFile(argv[2]);
   binFile.seekg(0, std::ios::beg);
-  while(binFile.good() && !binFile.eof())
+  while(!binFile.eof())
   {
     Eigen::Vector3f p;
     float refl;
-    binFile.read((char*) &p, 3*sizeof(float));
-    binFile.read((char*) &refl, sizeof(float));
+    binFile.read(static_cast<char*>(&p), 3 * sizeof(float));
+    binFile.read(static_cast<char*>(&refl), sizeof(float));
     if(binFile.good())
       txtFile << p.transpose() << " " << refl << std::endl;
   }
