f383bcbcb4a6cc89a500fa86d84cdc4d35265287
O B thuis <olaf.booij@xs4all.nl>
2021-02-13 13:02:51 +0100

    reading binary data to parse odometry dataset

---
+++ src/bintotxt.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<fstream>
+#include<Eigen/Core>
+
+int main(int argc, char* argv[])
+{
+  std::ifstream binFile(argv[1], std::ios::in | std::ios::binary);
+  std::ofstream txtFile(argv[2]);
+  binFile.seekg(0, std::ios::beg);
+  while(binFile.good() && !binFile.eof())
+  {
+    Eigen::Vector3f p;
+    float refl;
+    binFile.read((char*) &p, 3*sizeof(float));
+    binFile.read((char*) &refl, sizeof(float));
+    if(binFile.good())
+      txtFile << p.transpose() << " " << refl << std::endl;
+  }
+
+  return 0;
+}
+
