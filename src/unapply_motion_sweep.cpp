8e260e236be8f081b643ba2077876cb51ce70431
O B thuis <olaf.booij@xs4all.nl>
2021-03-10 17:00:00 +0100

    started implementing the use of liespline... but bumped into a nasty problem

--- src/unapply_motion_sweep.cpp
+++ src/unapply_motion_sweep.cpp
@@ -20,14 +20,23 @@ int main(int argc, char* argv[])
   // (first just assumed 2pi, later determine the heading-change using pose-date)
   // linearly interpolate pose and unapply
 
-  std::string day_path("../kitti/2011_09_30/");
-  std::string data_path(day_path + "2011_09_30_drive_0016_extract/");
-  auto imu_p_lidar = read_imu_velo_calib(day_path);
+  std::string data_path("../kitti/2011_09_30/2011_09_30_drive_0016_extract/");
+  auto imu_p_lidar = read_imu_velo_calib(data_path + "../");
   auto velo_time = read_velo_times(data_path);
   auto pose_time = read_pose_times(data_path);
   auto poses = read_poses(data_path, pose_time);
   for(auto& [time, pose]: poses)
     pose = poses.front().pose.inverse() * pose;
+
+  // I'm going to assume that oxts timing is constant at 10 ms. Let's check that this is the case
+  {
+    auto prev = poses.front().time;
+    for(auto [time, pose]: std::vector(poses.begin() + 1, poses.end()))
+    {
+      assert(time - prev - .01  < .001);
+      prev = time;
+    }
+  }
 
   ifstream sweepFile("00_odo.txt");
   int sweep_id = 3; // = 0 in odmetry
@@ -37,7 +46,14 @@ int main(int argc, char* argv[])
   {
     auto scan_time = (1 - delta) * sweep_time.start + delta * sweep_time.end;
     // taking nearest neighbour... I should interpolate
-    return std::lower_bound(poses.begin(), poses.end(), scan_time, [](auto pose, auto time){return pose.time < time;})->pose;
+    //auto closestPose = std::upper_bound(poses.begin(), poses.end(), scan_time, [](auto time, auto pose){return pose.time > time;});
+    auto closestPose = std::lower_bound(poses.begin(), poses.end(), scan_time, [](auto pose, auto time){return pose.time < time;}) - 1;
+    std::cout << closestPose->time - poses.front().time << " " << scan_time - poses.front().time << std::endl;
+    assert(closestPose - poses.begin() > 3);
+    assert(poses.end() -  closestPose > 1);
+    auto interpolatedPose = liespline::interpolate<liespline::se3>(closestPose - 3, scan_time - closestPose->time);
+    //return closestPose->pose;
+    return interpolatedPose;
   };
   auto world_p_imu_ref = interpolate_pose(poses, sweep_time, .75);
   auto world_p_lidar_ref = world_p_imu_ref * imu_p_lidar;
@@ -48,7 +64,7 @@ int main(int argc, char* argv[])
   double refl;
   while(sweepFile >> point_ref(0) >> point_ref(1) >> point_ref(2) >> refl)
   {
-    // correct point ... get lidar position
+    // correct point ... get lidar rotational position
     auto [probeId_r, position_r, distanceUncor_r, vertId__r] = sweepUncalibrator_orig(point_ref);
     auto delta = (position_r + M_PI) / (2 * M_PI);
     auto world_p_imu = interpolate_pose(poses, sweep_time, delta);
