b34f611a7d07cd91c16a8a0824d5c73575b6c132
O B thuis <olaf.booij@xs4all.nl>
2021-04-08 23:08:05 +0200

    trying to make odometry poses and oxts poses perfectly align

--- src/plot_oxts.cpp
+++ src/plot_oxts.cpp
@@ -8,6 +8,8 @@ int main(int argc, char* argv[])
   auto imu_p_lidar = read_imu_velo_calib(raw_data_path + "../");
   auto pose_time = read_pose_times(raw_data_path);
   auto poses = read_poses(raw_data_path, pose_time);
+  //decltype(poses) poses_(poses.begin() + 45, poses.end());
+  //poses = poses_;
   {
     for(auto& [time, pose]: poses)
       pose = imu_p_lidar.inverse() * pose;
