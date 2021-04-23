35a553ff21e109e4d710d55e7be5b1c4f95f6bd1
O B thuis <olaf.booij@xs4all.nl>
2021-04-23 22:59:24 +0200

    unzip script

--- sh/discretize_all_odo_raw_sets.sh
+++ sh/discretize_all_odo_raw_sets.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 mkdir -p odo_sweep_coordinates/{00,01,02,04,05,06,07,08,09,10}
-#for i in {0003..4543}; do ../bin/discretize_sweep $KITTI/2011_10_03/2011_10_03_drive_0027_extract/velodyne_points/data/000000$i.txt odo_sweep_coordinates/00/$(printf %04d $(("10#$i-3"))).txt; done
-#for i in {0004..1104}; do ../bin/discretize_sweep $KITTI/2011_10_03/2011_10_03_drive_0042_extract/velodyne_points/data/000000$i.txt odo_sweep_coordinates/01/$(printf %04d $(("10#$i-4"))).txt; done
-#for i in {0003..4663}; do ../bin/discretize_sweep $KITTI/2011_10_03/2011_10_03_drive_0034_extract/velodyne_points/data/000000$i.txt odo_sweep_coordinates/02/$(printf %04d $(("10#$i-3"))).txt; done
-for i in {0003..0273}; do ../bin/discretize_sweep $KITTI/2011_09_30/2011_09_30_drive_0016_extract/velodyne_points/data/000000$i.txt odo_sweep_coordinates/04/$(printf %04d $(("10#$i-3"))).txt; done
-#for i in {0003..2763}; do ../bin/discretize_sweep $KITTI/2011_09_30/2011_09_30_drive_0018_extract/velodyne_points/data/000000$i.txt odo_sweep_coordinates/05/$(printf %04d $(("10#$i-3"))).txt; done
-#for i in {0003..1103}; do ../bin/discretize_sweep $KITTI/2011_09_30/2011_09_30_drive_0020_extract/velodyne_points/data/000000$i.txt odo_sweep_coordinates/06/$(printf %04d $(("10#$i-3"))).txt; done
-#for i in {00??..110?}; do ../bin/discretize_sweep $KITTI/2011_09_30/2011_09_30_drive_0027_extract/velodyne_points/data/000000$i.txt odo_sweep_coordinates/07/$(printf %04d $(("10#$i-3"))).txt; done
-#for i in {1103..5173}; do ../bin/discretize_sweep $KITTI/2011_09_30/2011_09_30_drive_0028_extract/velodyne_points/data/000000$i.txt odo_sweep_coordinates/08/$(printf %04d $(("10#$i-1103"))).txt; done
-#for i in {0003..1593}; do ../bin/discretize_sweep $KITTI/2011_09_30/2011_09_30_drive_0033_extract/velodyne_points/data/000000$i.txt odo_sweep_coordinates/09/$(printf %04d $(("10#$i-3"))).txt; done
-#for i in {0003..1203}; do ../bin/discretize_sweep $KITTI/2011_09_30/2011_09_30_drive_0034_extract/velodyne_points/data/000000$i.txt odo_sweep_coordinates/10/$(printf %04d $(("10#$i-3"))).txt; done
+for i in {0003..4543}; do ../sh/discretize_zipped_sweep.sh 2011_10_03_drive_0027_extract $i 3    00; done
+for i in {0004..1104}; do ../sh/discretize_zipped_sweep.sh 2011_10_03_drive_0042_extract $i 4    01; done
+for i in {0003..4663}; do ../sh/discretize_zipped_sweep.sh 2011_10_03_drive_0034_extract $i 3    02; done
+for i in {0003..0273}; do ../sh/discretize_zipped_sweep.sh 2011_09_30_drive_0016_extract $i 3    04; done
+for i in {0003..2763}; do ../sh/discretize_zipped_sweep.sh 2011_09_30_drive_0018_extract $i 3    05; done
+for i in {0003..1103}; do ../sh/discretize_zipped_sweep.sh 2011_09_30_drive_0020_extract $i 3    06; done
+for i in {00??..110?}; do ../sh/discretize_zipped_sweep.sh 2011_09_30_drive_0027_extract $i 3    07; done
+for i in {1103..5173}; do ../sh/discretize_zipped_sweep.sh 2011_09_30_drive_0028_extract $i 1103 08; done
+for i in {0003..1593}; do ../sh/discretize_zipped_sweep.sh 2011_09_30_drive_0033_extract $i 3    09; done
+for i in {0003..1203}; do ../sh/discretize_zipped_sweep.sh 2011_09_30_drive_0034_extract $i 3    10; done
---
+++ sh/discretize_zipped_sweep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+dataset=$1
+framenr=$2
+startnr=$3
+odonr=$4
+
+date=${dataset:0:10}
+unzip $KITTIZIPS/$dataset.zip  $date/$dataset/velodyne_points/data/000000$framenr.txt
+../bin/discretize_sweep $date/$dataset/velodyne_points/data/000000$framenr.txt odo_sweep_coordinates/$odonr/$(printf %04d $(("10#$framenr-$startnr"))).txt
+rm $date/$dataset/velodyne_points/data/000000$framenr.txt
