9e3116f93b0b16aaeedd6c65d35953171eca8bd9
O B thuis <olaf.booij@xs4all.nl>
2021-04-24 22:24:17 +0200

    changed name to .bin to indicate it's binary files

--- sh/discretize_zipped_sweep.sh
+++ sh/discretize_zipped_sweep.sh
@@ -7,5 +7,5 @@ odonr=$4
 
 date=${dataset:0:10}
 unzip $KITTIZIPS/$dataset.zip  $date/$dataset/velodyne_points/data/000000$framenr.txt
-../bin/discretize_sweep $date/$dataset/velodyne_points/data/000000$framenr.txt odo_sweep_coordinates/$odonr/$(printf %04d $(("10#$framenr-$startnr"))).txt
+../bin/discretize_sweep $date/$dataset/velodyne_points/data/000000$framenr.txt odo_sweep_coordinates/$odonr/$(printf %04d $(("10#$framenr-$startnr"))).bin
 rm $date/$dataset/velodyne_points/data/000000$framenr.txt
