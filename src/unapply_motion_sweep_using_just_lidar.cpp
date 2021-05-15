dfd8d91500610e5dd1e1b7b11b3973439a901343
olaf booij in mist <olaf.booij@xs4all.nl>
2021-05-15 20:45:31 +0200

    squared error; a lot of hacky logging, to check what's going on...

--- src/unapply_motion_sweep_using_just_lidar.cpp
+++ src/unapply_motion_sweep_using_just_lidar.cpp
@@ -32,10 +32,10 @@ auto readSweep(auto&& file)
 int main(int argc, char* argv[])
 {
   using namespace ittik;
-  //ifstream sweepFile(argv[1]);
-  //ofstream outFile(argv[4]);
+  std::ifstream sweepFile(argv[1]);
+  //std::ofstream outFile(argv[4]);
 
-  const auto sweep = readSweep(std::ifstream("00_odo.txt"));
+  const auto sweep = readSweep(sweepFile);
 
 
   std::array<decltype(sweep.front().begin()), 64> lasersAtPoseStart;
@@ -55,29 +55,82 @@ int main(int argc, char* argv[])
   auto errorFunc = [&](auto laserPcloud)
   {
     Eigen::Vector2d error{0., 0.};
+    //static int it = 0; ++it;
     double stepangle = 2 * M_PI / 4000;
     for(auto [probeId, lasers]: enumerate(sweep))
     {
       for(auto laserIt = lasersAtPoseStart.at(probeId); laserIt != lasersAtPoseEnd.at(probeId); ++laserIt)
       {
-        auto pointLaser = laserPcloud * (*laserIt);
+        Eigen::Vector3d pointLaser = laserPcloud * (*laserIt);
         auto [position, distanceUncor] = unapply_calibration(pointLaser, kitti_probe_calibration().at(probeId));
         Eigen::Vector2d sample_error{position - std::lround(position / stepangle) * stepangle,
                                      vertical_angle_difference(pointLaser, kitti_probe_calibration().at(probeId))};
-        //error(0) += sample_error.norm();
-        error(0) += fabs(sample_error(0));
-        //error(1) += fabs(sample_error(1));
+        //std::cout << it << " " << position << " " << probeId << " " << std::lround(position / stepangle) * stepangle << " " << sample_error(1) << std::endl;
+        //if(fabs(sample_error(1)) < .005)
+        {
+          //error(0) += sample_error.norm();
+          error(0) += sample_error(0) * sample_error(0);
+          //error(1) += sample_error(1) * sample_error(1);
+          //std::cout << "e " << sample_error(1) << std::endl;
+        }
       }
     }
+    //error(0) = sqrt(error(0));
+    //error(1) = sqrt(error(1));
+    //error(0) *= 1000;
+    //error(1) *= 1000;
     return error;
   };
-  std::cout << errorFunc(liespline::Isometryd3::Identity()) << std::endl;
-  auto estimate = liespline::Isometryd3::Identity();
-  for(int i=9000;i--;)
-    std::cout << liespline::logse3(estimate).transpose() << " "
-              << errorFunc(estimate = gradient_descent_step(estimate, errorFunc)).transpose()  << std::endl;
-              // ...try out gaus-newton... -> levenberg marquardt
 
+  auto estimate = liespline::Isometryd3::Identity();
+
+  {
+    double stepangle = 2 * M_PI / 4000;
+    for(auto [probeId, lasers]: enumerate(sweep))
+      for(auto laserIt = lasersAtPoseStart.at(probeId); laserIt != lasersAtPoseEnd.at(probeId); ++laserIt)
+      {
+        Eigen::Vector3d pointLaser = estimate * (*laserIt);
+        auto [position, distanceUncor] = unapply_calibration(pointLaser, kitti_probe_calibration().at(probeId));
+        Eigen::Vector2d sample_error{position - std::lround(position / stepangle) * stepangle,
+                                     vertical_angle_difference(pointLaser, kitti_probe_calibration().at(probeId))};
+        static std::ofstream file("err");
+        file << " " << position << " " << probeId << " " << std::lround(position / stepangle) * stepangle << " " << sample_error(1) << std::endl;
+      }
+  }
+  //errorFunc(estimate);
+  for(int i=1e3;i--;)
+  //for(int i=1;i--;)
+  {
+    std::cout << liespline::logse3(estimate).transpose() << " "
+              << errorFunc(estimate).transpose() << std::endl;
+    estimate = gradient_descent_step(estimate, errorFunc);
+  }
+
+  {
+    std::array<decltype(sweep.front().begin()), 64> lasersAtPoseStart;
+    std::array<decltype(sweep.front().begin()), 64> lasersAtPoseEnd;
+    for(auto [probeId, lasers]: enumerate(sweep))
+      lasersAtPoseEnd.at(probeId) = lasersAtPoseStart.at(probeId) = lasers.begin();
+
+    for(auto [probeId, lasers]: enumerate(sweep))
+    {
+      auto& laserEnd = lasersAtPoseEnd.at(probeId);
+      while(atan2((*laserEnd)(1), (*laserEnd)(0)) < .2)
+        ++laserEnd;
+      //std::cout << laserEnd - lasersAtPoseStart.at(probeId) << std::endl;
+    }
+    double stepangle = 2 * M_PI / 4000;
+    for(auto [probeId, lasers]: enumerate(sweep))
+      for(auto laserIt = lasersAtPoseStart.at(probeId); laserIt != lasersAtPoseEnd.at(probeId); ++laserIt)
+      {
+        Eigen::Vector3d pointLaser = estimate * (*laserIt);
+        auto [position, distanceUncor] = unapply_calibration(pointLaser, kitti_probe_calibration().at(probeId));
+        Eigen::Vector2d sample_error{position - std::lround(position / stepangle) * stepangle,
+                                     vertical_angle_difference(pointLaser, kitti_probe_calibration().at(probeId))};
+        static std::ofstream file("err_after");
+        file << " " << position << " " << probeId << " " << std::lround(position / stepangle) * stepangle << " " << sample_error(1) << std::endl;
+      }
+  }
 
 
   // use some silly generic function to minimize the error by changing the pose...
