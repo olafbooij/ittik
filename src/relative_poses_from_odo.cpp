a5514e7062639eafa5cb6261874e848dc78a078e
olaf booij in mist <olaf.booij@xs4all.nl>
2021-08-29 21:06:42 +0200

    moved to own bin

--- src/relative_poses_from_odo.cpp
+++ src/relative_poses_from_odo.cpp
@@ -1,5 +1,6 @@
+#include<iostream>
 #include"io.hpp"
-#include"liespline/se3_plot.hpp"
+#include"liespline/se3.hpp"
 
 int main(int argc, char* argv[])
 {
@@ -11,9 +12,12 @@ int main(int argc, char* argv[])
     pose = cam_p_lidar.inverse() * pose * cam_p_lidar;
 
   std::ofstream file(argv[3]);
+  auto& prev_pose = poses.front();
   for(auto& pose: poses)
-    liespline::plot_se3(pose, file, .05);
-
+  {
+    std::cout << liespline::logse3(prev_pose.inverse() * pose).transpose() / (2 * M_PI) << std::endl;
+    prev_pose = pose;
+  }
   return 0;
 }
 
