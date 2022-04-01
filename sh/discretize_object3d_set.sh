6e162d8887f2ddf5ea8942afddd1bd5c9106cbd1
O B thuis <olaf.booij@xs4all.nl>
2022-04-01 22:54:59 +0200

    let me zip per sweep for now

--- sh/discretize_object3d_set.sh
+++ sh/discretize_object3d_set.sh
@@ -7,6 +7,7 @@ for framenr in {0000..7481}; do
   unzip $zip training/velodyne/00$framenr.bin
   ../bin/bintotxt  training/velodyne/00$framenr.bin training/velodyne/00$framenr.txt
   ../bin/discretize_sweep training/velodyne/00$framenr.txt object_sweep_coordinates/training/00$framenr.bin
+  gzip object_sweep_coordinates/training/00$framenr.bin
   rm training/velodyne/00$framenr.*
 done
 
@@ -15,5 +16,6 @@ for framenr in {0000..7517}; do
   unzip $zip testing/velodyne/00$framenr.bin
   ../bin/bintotxt  testing/velodyne/00$framenr.bin testing/velodyne/00$framenr.txt
   ../bin/discretize_sweep testing/velodyne/00$framenr.txt object_sweep_coordinates/testing/00$framenr.bin
+  gzip object_sweep_coordinates/testing/00$framenr.bin
   rm testing/velodyne/00$framenr.*
 done
